2c1bbcfdbf5b5a35e2a9596cd4fc6dc58492e74a
dmitry.tkachenko <dmitry.tkachenko@talkremit.com>
2019-12-21 18:42:39 +0300

    Add solution folders

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -24,7 +24,7 @@ namespace EmptyService.WebApi
                             .AddJsonFile("appsettings.json")
                             .AddEnvironmentVariables()
                             .Build();
-            
+
             shouldStartJob = bool.Parse(Configuration.GetSection("JobSettings:ShouldStart").Value);
         }
 
